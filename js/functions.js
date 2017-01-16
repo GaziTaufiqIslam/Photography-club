b7ab2d169b58481e5fa4d3e38977144e0eb6a66f
GaziTaufiqIslam <gzidev@gmail.com>
2017-01-17 01:57:06 +0530

    blog section has a new javascript carasoul now

--- js/functions.js
+++ js/functions.js
@@ -9,33 +9,42 @@ $( document ).ready(function(){
 $(window).scroll(function () {
   var wScroll= $(this).scrollTop();
   var winH = $(window).height();
-    $(".scroll-parallax").css("background-position","50% " + ( -((wScroll / 5))) + "px");
+  $(".scroll-parallax").css("background-position","50% " + ( -((wScroll / 5))) + "px");
   if(wScroll > winH) {
-  $('.navtop').addClass('show');
-}
-else {
+    $('.navtop').addClass('show');
+  }
+  else {
     $('.navtop').removeClass('show');
-}
+  }
 });
 
 lightbox.option({
-      'resizeDuration': 300,
-      'wrapAround': true,
-    })
+  'resizeDuration': 300,
+  'wrapAround': true,
+})
 
 function blogStuff() {
   $('.blog-unit').first().addClass('active');
 
-  $('.blog-control-next').on('click', function(){
+  $('.blog-control-next, .blog-control-prev').on('click', function(){
     var $this= $(this),
-        curActiveBlog = $('.blog-belt').find('.active'),
-        position = $('.blog-belt').children().index(curActiveBlog),
-        blogNum = $('.blog-unit').length;
-if(position<(blogNum-1)){
+    curActiveBlog = $('.blog-belt').find('.active'),
+    position = $('.blog-belt').children().index(curActiveBlog),
+    blogNum = $('.blog-unit').length;
+    if($this.hasClass('blog-control-next')){
+      if(position<(blogNum-1)){
         $('.active').removeClass('active').next().addClass('active');
-}
-else {
-$('.blog-unit').removeClass('active').first().addClass('active');
-}
-  });
-}
+      }
+      else {
+        $('.blog-unit').removeClass('active').first().addClass('active');
+      }}
+      else {
+        if(position>0){
+          $('.active').removeClass('active').prev().addClass('active');
+        }
+        else {
+          $('.blog-unit').removeClass('active').last().addClass('active');
+        }
+      }
+    });
+  }
